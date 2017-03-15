099d9514d2788896f6b135855d89fbd800fa21ae
Ivan Bosnic <ivanbosnic@univille.br>
2017-03-14 22:24:21 -0300

    bovespa

--- bovespa/src/com/bovespa/Main.java
+++ bovespa/src/com/bovespa/Main.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 
 public class Main {
 
+	
 	public static void main(String[] args) throws Exception {
 
 		Stock[] stocks = new Stock[20];
@@ -15,13 +16,11 @@ public class Main {
 		Stream<String> lines = Files.lines(Paths.get("C:/eclipseneon2/COTAHIST_A2016_TEST.txt"));
 
 		lines.forEach(line -> {
-
 			if (line.startsWith("01")) {
 				Stock stock = new Stock();
 				stock.setCode(line.substring(12, 23).trim());
 				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
 				stock.setDate(LocalDate.parse(line.substring(2, 10), dtf));
-
 				System.out.println(line.substring(2, 10).trim());
 			}
 		});
