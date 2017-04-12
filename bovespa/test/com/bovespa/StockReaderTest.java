1704784cf3c82e8c2246b1e45e4d79c3c3f35ce2
Ivan Bosnic <ivanbosnic@univille.br>
2017-04-11 22:08:12 -0300

    linked list

--- bovespa/test/com/bovespa/StockReaderTest.java
+++ bovespa/test/com/bovespa/StockReaderTest.java
@@ -2,22 +2,26 @@ package com.bovespa;
 
 import org.junit.Test;
 
+import estd.SLinkedList;
+
 public class StockReaderTest {
 
 	@Test
-	public void readTest() throws Exception {
+	public void readAsLinkedListTest() throws Exception {
 		StockReader stockReader = new StockReader();
-		Stock[] stocks = stockReader.read("C:/eclipseneon2/COTAHIST_A2016.txt");
-
 		long start = System.currentTimeMillis();
-
-		Sorter sorter = new Sorter();
-		sorter.sort(stocks);
-
+		SLinkedList<Stock> linkedList = stockReader.readAsLinkedList("C:/eclipseneon2/COTAHIST_A2016.txt");
 		long end = System.currentTimeMillis();
-
 		System.out.println((end - start)/1000);
-		
-	}
+	}	
+	
+	@Test
+	public void readAsArrayTest() throws Exception {
+		StockReader stockReader = new StockReader();
+		long start = System.currentTimeMillis();
+		Stock[] stocks = stockReader.readAsArray("C:/eclipseneon2/COTAHIST_A2016.txt");
+		long end = System.currentTimeMillis();
+		System.out.println((end - start)/1000);
+	}	
 
 }
