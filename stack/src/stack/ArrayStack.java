7a690dba9aff9965e3f8aac1fa62c1bd262656ec
Ivan Bosnic <ivanbosnic@univille.br>
2017-05-15 19:43:31 -0300

    resize

--- stack/src/stack/ArrayStack.java
+++ stack/src/stack/ArrayStack.java
@@ -2,36 +2,53 @@ package stack;
 
 public class ArrayStack<E> implements Stack<E> {
 
-	private E[] array = (E[]) new Object[20];
+	private E[] array;
 
 	private int position = 0;
-	
-	public ArrayStack() {
 
+	public ArrayStack() {
+		array = (E[]) new Object[20];
 	}
 
 	public ArrayStack(int size) {
-
+		if (size <= 0) {
+			throw new IllegalArgumentException();
+		}
+		array = (E[]) new Object[size];
 	}
-
 
 	@Override
 	public void push(E e) {
+		
+		if(position == array.length) {
+			E[] temp = (E[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, temp, 0, array.length);
+			array = temp;
+		}
+		
 		array[position] = e;
 		position++;
 	}
 
 	@Override
 	public E top() {
-		return array[position - 1];
+		if (isEmpty()) {
+			return null;
+		} else {
+			return array[position - 1];
+		}
 	}
 
 	@Override
 	public E pop() {
-		E temp = array[position - 1];
-		array[position - 1] = null;
-		position--;
-		return temp;
+		if (isEmpty()) {
+			return null;
+		} else {
+			E temp = array[position - 1];
+			array[position - 1] = null;
+			position--;
+			return temp;
+		}
 	}
 
 	@Override
