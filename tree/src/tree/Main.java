3fd1786ce4866528792665a4fdf7d3cd8106face
Ivan Bosnic <ivanbosnic@univille.br>
2017-08-29 21:40:52 -0300

    tree

--- tree/src/tree/Main.java
+++ tree/src/tree/Main.java
@@ -1,20 +1,42 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
 
 	public static void main(String[] args) {
+		TreeNode<String> root = new TreeNode<>("A");
+		TreeNode<String> b = root.addChild("B");
+		b.addChild("H");
+		TreeNode<String> c = root.addChild("C");
+		c.addChild("D");
+		c.addChild("E");
+		TreeNode<String> f = c.addChild("F");
+		f.addChild("G");
 		
-		TreeNode<String> root = new TreeNode<>("Raiz");
-		root.addChild("F1");
-		TreeNode<String> f2 = root.addChild("F2");
-		f2.addChild("F3");
-		f2.addChild("F4");
-		
-		for (TreeNode<String> treeNode : root.getChildren()) {
-			System.out.println(treeNode);
+		bfs(root);
+	}
+	
+	public static void bfs(TreeNode<String> node) {
+		Queue<TreeNode<String>> queue = new LinkedList<>();
+		queue.add(node);
+		while(!queue.isEmpty()){
+			TreeNode<String> processing = queue.poll();
+			System.out.println(processing);
+			for (TreeNode<String> treeNode : processing.getChildren()) {
+				queue.add(treeNode);
+			}
 		}
-		
-		System.out.println(root);
+	}
+	
+	public static void dfs(TreeNode<String> node) {
+		System.out.println(node);
+		List<TreeNode<String>> children = node.getChildren();
+		for (TreeNode<String> treeNode : children) {
+			dfs(treeNode);
+		}
 	}
 	
 }
